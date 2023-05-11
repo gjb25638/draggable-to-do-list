ed94f9ccde9e315cf1e4ab4c98b75b9cc26a1a0c
Ellis Huang <ellishuang@ch-si.com.tw>
2023-05-11 17:14:08 +0800

    update

---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div class="home__container">
+      <div class="home__container__text">
+        哈哈哈哈哈
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.home {
+  &__container {
+    @apply w-full h-300px bg-blue-500;
+
+    &__text {
+      @apply text-red-500;
+    }
+  }
+}
+</style>
