00f2695a1f2879a939681e11a1c47d799f16a1ae
Ellis Huang <ellishuang@ch-si.com.tw>
2023-07-14 16:52:36 +0800

    fix: hide some locale code from el-plus

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,9 +1,9 @@
 import tool from '@/utils/tool'
 import { createI18n } from 'vue-i18n'
-import el_zh_tw from 'element-plus/lib/locale/lang/zh-tw'
-import el_zh_cn from 'element-plus/lib/locale/lang/zh-cn'
-import el_en from 'element-plus/lib/locale/lang/en'
-import el_ja from 'element-plus/lib/locale/lang/ja'
+// import el_zh_tw from 'element-plus/lib/locale/lang/zh-tw'
+// import el_zh_cn from 'element-plus/lib/locale/lang/zh-cn'
+// import el_en from 'element-plus/lib/locale/lang/en'
+// import el_ja from 'element-plus/lib/locale/lang/ja'
 
 import zh_tw from './lang/zh-tw.js'
 import zh_cn from './lang/zh-cn.js'
@@ -12,19 +12,19 @@ import ja from './lang/ja.js'
 
 const messages = {
   'zh-tw': {
-    el: el_zh_tw,
+    // el: el_zh_tw,
     ...zh_tw
   },
   'zh-cn': {
-    el: el_zh_cn,
+    // el: el_zh_cn,
     ...zh_cn
   },
   en: {
-    el: el_en,
+    // el: el_en,
     ...en
   },
   ja: {
-    el: el_ja,
+    // el: el_ja,
     ...ja
   }
 }
@@ -34,7 +34,7 @@ const i18n = createI18n({
   locale: tool.data.get('APP_LANG') || 'zh-tw',
   fallbackLocale: 'zh-tw',
   globalInjection: true,
-  messages,
+  messages
 })
 
 export default i18n
